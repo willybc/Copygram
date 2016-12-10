c434fe737bbaac05a9dd19f6639b2db805b1f859
Willy Bardales <willybc06@gmail.com>
2016-12-10 19:50:26 -0300

    Subiendo la foto a nuestro servidor web

--- src/homepage/template.js
+++ src/homepage/template.js
@@ -2,12 +2,13 @@ var yo = require('yo-yo');
 var layout = require('../layout');
 var picture = require('../picture-card');
 var translate = require('../translate');
+var request = require('superagent');
 
 module.exports = function (pictures) {
   var el = yo`<div class="container timeline">
     <div class="row">
       <div class="col s12 m10 offset-m1 l8 offset-l2 center-align">
-        <form enctype="multipart/form-data" class="form-upload" id="formUpload">
+        <form enctype="multipart/form-data" class="form-upload" id="formUpload" onsubmit=${onsubmit}>
           <div id="fileName" class="fileUpload btn btn-flat cyan">
             <span><i class="fa fa-camera" aria-hidden="true"></i> ${translate.message('upload-picture')}</span>
             <input name="picture" id="file" type="file" class="upload" onchange=${onchange}  />
@@ -41,6 +42,17 @@ module.exports = function (pictures) {
     toggleButtons();
   }
 
+  function onsubmit(ev) {
+    ev.preventDefault();
+
+    var data = new FormData(this);
+    request
+      .post('/api/pictures')
+      .send(data)
+      .end(function (err, res) {
+        console.log(arguments);
+      })
+  }
   return layout(el);
 }
 
