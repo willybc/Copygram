9f47cf77b362fca854b0d2eab1e94471654ac967
Willy Bardales <willybc06@gmail.com>
2016-12-13 14:35:52 -0300

    Desafio 3

--- src/user-page/index.js
+++ src/user-page/index.js
@@ -15,7 +15,20 @@ page('/:username', loadUser, header, function (ctx, next) {
   var main = document.getElementById('main-container');
   title(`Platzigram - ${ctx.user.username}`);
   empty(main).appendChild(template(ctx.user));
+  $('.modal.trigger').learnModal();
 });
+
+page('/:username/:id', loadUser, header, function (ctx, next) {
+  var main = document.getElementById('main-container');
+  title(`Platzigram - ${ctx.user.username}`);
+  empty(main).appendChild(template(ctx.user));
+  $(`#modal${ctx.params.id}`).openModal({
+    complete: function () {
+      page(`/${ctx.params.username}`)
+    }
+  });
+});
+
 
 async function loadUser (ctx, next) {
   try {
