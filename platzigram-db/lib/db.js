9c5deae0235498d4edbb0171169594733ea839a5
Willy Bardales <willybc06@gmail.com>
2016-12-19 13:35:43 -0300

    Creacion de test para RethinkDB , almacenamiento de ruta de imagenes en RethinkDB

--- platzigram-db/lib/db.js
+++ platzigram-db/lib/db.js
@@ -17,11 +17,14 @@ class Db {
     this.port = options.port || defaults.port
     this.db = options.db || defaults.db
   }
+
   connect (callback) {
     this.connection = r.connect({
       host: this.host,
       port: this.port
     })
+
+    this.connected = true
 
     let db = this.db
     let connection = this.connection
@@ -33,12 +36,10 @@ class Db {
       if (dbList.indexOf(db) === -1) {
         yield r.dbCreate(db).run(conn)
       }
-      
       let dbTables = yield r.db(db).tableList().run(conn)
       if (dbTables.indexOf('images') === -1) {
         yield r.db(db).tableCreate('images').run(conn)
       }
-
       if (dbTables.indexOf('users') === -1) {
         yield r.db(db).tableCreate('users').run(conn)
       }
@@ -47,7 +48,42 @@ class Db {
     })
 
     return Promise.resolve(setup()).asCallback(callback)
+  }
 
+  disconnect (callback) {
+    if (!this.connected) {
+      return Promise.reject(new Error('not connected')).assCallback(callback)
+    }
+
+    this.connected = false
+    return Promise.resolve(this.connection)
+    .then((conn) => conn.close())
+  }
+
+  saveImage (image, callback) {
+    if (!this.connect) {
+      return Promise.reject(new Error('not connected')).Callback(callback)
+    }
+
+    let connection = this.connection
+    let db = this.db
+
+    let tasks = co.wrap(function * () {
+      let conn = yield connection
+      image.createdAt = new Date()
+
+      let result = yield r.db(db).table('images').insert(image).run(conn)
+
+      if (result.errors > 0) {
+        return Promise.reject(new Error(result.first_error))
+      }
+
+      image.id = result.generated_keys[0]
+
+      return Promise.resolve(image)
+    })
+
+    return Promise.resolve(tasks()).asCallback(callback)
   }
 }
 
