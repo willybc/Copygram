7e10be2acdeaa92fb0e2da0a80ccd691f8e85787
Willy Bardales <willybc06@gmail.com>
2017-01-06 18:02:20 -0300

    Introduccion a los Microservicios

--- README.md
+++
@@ -1,4 +0,0 @@
-# Platzigram
-https://willybc.github.io/Platzigram/
-
-Proyecto de aprendizaje , gracias a https://platzi.com
---
+++ platzigram-api/test/pictures-test.js
@@ -0,0 +1,21 @@
+'use strict'
+
+import test from 'ava'
+import micro, { send } from 'micro'
+import uuid from 'uuid-base62'
+import listen from 'test-listen'
+import request from 'request-promise'
+
+test('GET /:id', async t => {
+  let id = uuid.v4()
+  let srv = micro(async (req, res) => {
+    send(res, 200, { id: id })
+  })
+
+  let url = await listen(srv)
+  let body = await request({ url: url, json: true })
+  t.deepEqual(body, { id })
+})
+
+test.todo('POST /')
+test.todo('POST /:id/like')
