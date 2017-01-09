d89380c73ba2a47aa79628f0cd8e7d18ddefd704
Willy Bardales <willybc06@gmail.com>
2017-01-09 08:32:05 -0300

    Creacion de nuestro cliente

---
+++ platzigram-client/lib/client.js
@@ -0,0 +1,23 @@
+'use strict'
+
+class Client {
+
+  getPicture () {}
+
+  savePicture () {}
+
+  likePicture () {}
+
+  listPictures () {}
+
+  listPicturesByTag () {}
+
+  saveUser () {}
+
+  getUser () {}
+
+  auth () {}
+}
+
+module.exports = Client
+
---
+++ platzigram-client/test/client-test.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const test = require('ava')
+const platzigram = require('../')
+// const fixtures = require('./fixtures')
+
+test('client', t => {
+  const client = platzigram.createClient()
+
+  t.is(typeof client.getPicture, 'function')
+  t.is(typeof client.savePicture, 'function')
+  t.is(typeof client.likePicture, 'function')
+  t.is(typeof client.listPictures, 'function')
+  t.is(typeof client.listPicturesByTag, 'function')
+  t.is(typeof client.saveUser, 'function')
+  t.is(typeof client.getUser, 'function')
+  t.is(typeof client.auth, 'function')
+})
